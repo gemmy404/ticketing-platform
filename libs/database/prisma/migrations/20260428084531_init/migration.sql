4c67b8bb9f617916ccdcc512c63ee55584ecd480
Mahmoud Gamal <mahmoudgemmy581@gmail.com>
2026-05-04 22:55:45 +0300

    Initialize Prisma migration for `users` table and `user_role` enum setup

---
+++ libs/database/prisma/migrations/20260428084531_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateEnum
+CREATE TYPE "user_role" AS ENUM ('USER', 'ORGANIZER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "role" "user_role" NOT NULL DEFAULT 'USER',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
